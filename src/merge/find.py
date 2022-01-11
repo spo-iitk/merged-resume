0ff35a11eacb1e51ae62b52ee8d32d55ff963859
Harshit Raj <harshitr20@iitk.ac.in>
2022-01-11 15:17:24 +0530

    Add find function for pdf folders

---
+++ src/merge/find.py
@@ -0,0 +1,20 @@
+from os import path, listdir
+from pathlib import Path
+
+"""
+Find zip files.
+"""
+
+# get root path
+def root_dir() -> str:
+    return Path(path.dirname(__file__)).parent.parent.absolute()
+
+
+# find all folders having pdf files in tmp directory
+def find() -> list[str]:
+    pdf_path = path.join(root_dir(), "tmp")
+    try:
+        return listdir(pdf_path)
+    except:
+        print("No pdf files found in tmp directory.")
+        return []
