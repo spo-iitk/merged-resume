63434df033e7d2dfb0330e7841338a673fa0f1c7
Harshit Raj <harshitr20@iitk.ac.in>
2022-01-11 15:47:53 +0530

    Add main file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 from unzip.unzip import unzip, cleanup
 from merge.merge import merge
 
-if __name__ == "__main__":
-    unzip()
+
+def main():
+    files = unzip()
+    for file in files:
+        print("Unzipping file ", file, "...")
+    if len(files) == 0:
+        print("No zip files in _inout directory. Exiting program.")
+        return
+
+    print("Merging unzipped PDF...")
     merge()
+
+    print("Cleaning up unzipped files...")
     cleanup()
+
+    print("Done! Find merged files in _inout directory.")
+
+
+if __name__ == "__main__":
+    main()
